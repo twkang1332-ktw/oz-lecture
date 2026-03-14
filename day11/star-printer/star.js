80f9867f2dfe842ba364803b22a5c19bfdeff67b
twkang1332-ktw <twkang1332@gmail.com>
2026-03-14 16:43:40 +0900

    star.js

--- day11/star-printer/star.js
+++ day11/star-printer/star.js
@@ -1,22 +1,57 @@
 const maxStars = 10;
 
-while (true) {
-    let input = prompt("Enter the number of stars (1-10)");
-    let count = Number(input);
-
-    if (isNaN(count) || count < 1 || count > maxStars) {
-        console.log("Invalid input! Enter a number between 1 and 10.");
-        continue;
-    } else {
-        printStars(count); 
-        break;
-    }
-}
-
-function printStars (count = 1) {
+// 1. 가로 별 출력
+function printStars (count) {
     var stars = "";
     for (let i = 0; i < count; i++) {
         stars += "*";
 }
 console.log(stars);
+}
+
+// 2. 역순 별 출력
+function reverseStars(count) {
+  for (let i = count; i > 0; i--) {
+    var stars = "";
+
+    for (let j = 0; j < i; j++) {
+      stars += "*";
+    }
+
+    console.log(stars);
+  }e
+}
+
+// 3. 사각형 별 출력
+const printSquare = (count) => {
+    var stars = "";
+    for (let i = 0; i < count; i++) {
+        for (let j = 0; j < count; j++) {
+            stars += "*";
+        }
+            stars += '\n'
+    }
+    console.log(stars);
+}
+
+while (true) {
+    let input = prompt("Enter the number of stars (1-10)");
+    let count = Number(input);      // 문자열 -> 숫자로 바꿔주기
+
+    if (isNaN(count) || count < 1 || count > maxStars) {
+        console.log("Invalid input! Enter a number between 1 and 10.");
+        continue;       // 숫자가 아니거나, 1보다 작거나, maxStars(10)보다 크면, 콘솔로그 띄운 뒤 처음부터 다시
+    } else {
+        
+        console.log("=== 1. 가로 별 출력 ===");
+        printStars(count);
+
+        console.log("=== 2. 역순 별 출력 ===");
+        reverseStars(count);
+
+        console.log("=== 3.사각형 별 출력 ===");
+        printSquare(count);
+
+        break;
+    }
 }
